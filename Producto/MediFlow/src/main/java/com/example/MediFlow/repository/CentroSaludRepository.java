ed74bfec76b2039fa2eeefc88b9f2107ebeb2305
SebastianEsparza18 <sesparza@miucsh.cl>
2026-04-23 19:44:40 -0400

    Add files via upload

---
+++ Producto/MediFlow/src/main/java/com/example/MediFlow/repository/CentroSaludRepository.java
@@ -0,0 +1,9 @@
+package com.example.MediFlow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.MediFlow.model.CentroSalud;
+
+public interface CentroSaludRepository extends JpaRepository<CentroSalud, Long>{
+
+}
