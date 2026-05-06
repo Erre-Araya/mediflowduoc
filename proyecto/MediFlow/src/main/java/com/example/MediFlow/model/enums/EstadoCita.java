ee76c59f14d12330b231f010121cfc7b1fb09594
AO-Alumno <AO-Alumno@duoc.acad>
2026-05-05 20:35:10 -0400

    cambios varios

--- proyecto/MediFlow/src/main/java/com/example/MediFlow/model/enums/EstadoCita.java
+++
@@ -1,9 +0,0 @@
-package com.example.MediFlow.model.enums;
-
-public enum EstadoCita {
-    AGENDADA,
-    CONFIRMADA,
-    CANCELADA,
-    ASISTIDA,
-    NO_ASISTIDA
-}
---
+++ proyecto/mi-app-mediflow/mi-app-mediflow/src/components/Header.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+
+  return (
+    <header className="header">
+      <h1 className="logo">Mediflow</h1>
+
+      <nav className="nav">
+        <Link to="/home">Inicio</Link>
+
+        {user?.rol === "PACIENTE" && (
+          <>
+            <Link to="/appointments">Mis citas</Link>
+            <Link to="/create">Agendar</Link>
+          </>
+        )}
+
+        {user?.rol === "PROFESIONAL" && (
+          <>
+            <Link to="/profesional/citas">Mis citas</Link>
+            <Link to="/patients">Pacientes</Link>
+          </>
+        )}
+
+        {user?.rol === "ADMIN" && (
+          <>
+            <Link to="/professionals">Profesionales</Link>
+            <Link to="/patients">Pacientes</Link>
+          </>
+        )}
+        {user && (
+          <button onClick={handleLogout}>
+            Salir
+          </button>
+        )}
+        
+      </nav>
+    </header>
+  );
+}
