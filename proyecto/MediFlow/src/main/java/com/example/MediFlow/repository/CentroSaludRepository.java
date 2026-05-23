e2178fa538b6b65682c5e47025b0978e060d3aa0
SebastianEsparza18 <sesparza@miucsh.cl>
2026-05-23 16:21:05 -0400

    Delete proyecto directory

--- proyecto/MediFlow/src/main/java/com/example/MediFlow/repository/CentroSaludRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MediFlow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.MediFlow.model.CentroSalud;
-
-public interface CentroSaludRepository extends JpaRepository<CentroSalud, Long>{
-
-}
