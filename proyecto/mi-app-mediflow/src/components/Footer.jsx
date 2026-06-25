449745743086e872e3c9092ef4af5d1f1c1d6006
SebastianEsparza18 <sesparza@miucsh.cl>
2026-06-25 15:44:42 -0400

    Delete proyecto directory

--- proyecto/mi-app-mediflow/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      © {new Date().getFullYear()} MediFlow · Sistema de gestión de citas médicas
-    </footer>
-  );
-}
--- proyecto/mi-app-mediflow/src/pages/NotFound.jsx
+++
@@ -1,3 +0,0 @@
-export default function NotFound() {
-  return <h2>Página no encontrada</h2>;
-}
